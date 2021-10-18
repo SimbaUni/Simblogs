92ad8bbf72feaa2f06571b611e82f62894117c0a
SimbaUni <simon_love@live.co.uk>
2021-10-18 01:45:13 +0100

    modified:   package-lock.json 	modified:   src/App.vue 	new file:   src/components/Footer.vue 	new file:   src/components/Navigation.vue 	modified:   src/firebase/firebaseInit.js 	modified:   src/views/Home.vue

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyD4Pj2c8VZ5Mx1MKEs8QGv_JIfnarTeHY4",
+  
+    authDomain: "simblogs-2cac2.firebaseapp.com",
+  
+    projectId: "simblogs-2cac2",
+  
+    storageBucket: "simblogs-2cac2.appspot.com",
+  
+    messagingSenderId: "340765239284",
+  
+    appId: "1:340765239284:web:7cde51bab8314b0dcbfba1",
+  
+    measurementId: "G-HL3N78Q3SS"
+  
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export {timestamp};
+export default firebaseApp.firestore();
+  

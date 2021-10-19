4b14b441635c3059f212d0d81c983556e09334da
SimbaUni <simon_love@live.co.uk>
2021-10-19 19:03:11 +0100

    Added route and view of blogs

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sampleBlogCards: [
+      { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "18 Oct, 2021" },
+      { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "18 Oct, 2021" },
+      { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "18 Oct, 2021" },
+      { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "18 Oct, 2021" },
+    ],
+    editPost: null
   },
   mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+    }
   },
   actions: {
   },
